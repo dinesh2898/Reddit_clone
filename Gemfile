897952b74ca3531c20eb1a9cef8f99fa40664dda
Dinesh <dinesh2898@gmail.com>
2019-08-11 15:37:02 +0530

    Added and setup acts_as_votable

--- Gemfile
+++ Gemfile
@@ -32,3 +32,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise', '~> 4.2'
 gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
+gem 'acts_as_votable', '~> 0.10.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :links
+  #devise_for :users
+  devise_for :users do
+  	get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  resources :links do
+  	member do
+  		get "like", to: "links#upvote"
+  		get "dislike", to: "links#downvote"
+  	end
+  end
 
   root to: "links#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
